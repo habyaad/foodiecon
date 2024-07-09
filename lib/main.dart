b2aa7eb65cc51702abbd6930597c20504f37aa24
habyaad <adeomotoshan@gmail.com>
2024-07-09 14:11:26 +0100

    feat: implement screen utils

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
+    ScreenUtilInit(
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        // Use builder only if you need to use library outside ScreenUtilInit context
+        builder: (_, child) {
+          return GetMaterialApp(
+            title: "Application",
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(textTheme: GoogleFonts.interTextTheme()),
+            initialRoute: AppPages.INITIAL,
+            getPages: AppPages.routes,
+          );
+        }),
   );
 }
