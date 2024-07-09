4fc76bbccbcdc480f3990f76378c206ac23ee0ff
habyaad <adeomotoshan@gmail.com>
2024-07-09 14:08:06 +0100

    feat: create app color palette

---
+++ lib/app/data/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors{
+  AppColors._();
+  static const appOrange = Color(0xFFFE8C00);
+  static const appGrey = Color(0xFFC2C2C2);
+  static const appGrey2 = Color(0xFF878787);
+  static const lightGrey = Color(0xFFD6D6D6);
+  static const appBlack = Color(0xFF101010);
+}
