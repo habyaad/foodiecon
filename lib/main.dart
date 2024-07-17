bbd5b25e5e99b20cf0203e90386fa8f3631e4325
habyaad <adeomotoshan@gmail.com>
2024-07-17 15:50:16 +0100

    feat: initialize supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+// load env
+  await dotenv.load();
+// initialize supabase
+  String supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
+  String supabaseKey = dotenv.env['SUPABASE_KEY'] ?? '';
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
+
   runApp(
     ScreenUtilInit(
         designSize: const Size(375, 812),
